2c3bad36a83210e1f0dfe450a8f3eb9f3ff765f5
Егор Зеленский <i@kha0s.ru>
2014-06-11 22:59:28 +0400

    Added UDP socket server implementation.

---
+++ Tesla.Core/Net/UdpServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Tesla.Net
+{
+    using HandlerFunc = Func<byte[], IPEndPoint, Task>;
+
+    public sealed class UdpSocketServer
+        : SocketServerBase<HandlerFunc>
+    {
+        public UdpSocketServer(HandlerFunc handlerFunc, IPAddress ip, int port)
+            : base(handlerFunc, ip, port)
+        {
+            ListenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        }
+
+        public UdpSocketServer(HandlerFunc handlerFunc, int port)
+            : this(handlerFunc, IPAddress.Any, port)
+        { }
+
+        public UdpSocketServer(HandlerFunc handlerFunc)
+            : this(handlerFunc, 0)
+        { }
+
+        protected override void BindSocket()
+        {
+            ListenerSocket.Bind(LocalEndPoint);
+            LocalEndPoint = (IPEndPoint) ListenerSocket.RemoteEndPoint;
+        }
+
+        protected override async Task<Action> AcceptClient()
+        {
+            var packet = await ListenerSocket.ReceiveAsync();
+            return async () =>
+            {
+                try
+                {
+                    await HandlerFunc(packet.Item1, packet.Item2);
+                }
+                catch (Exception e)
+                {
+                    Trace.TraceWarning("UDP Handler exception: {0}.", e.Message);
+                }
+            };
+        }
+    }
+}
