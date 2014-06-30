c2ea999561fde575fbdc9552225596cf9e4a75fb
Егор Зеленский <i@kha0s.ru>
2014-06-30 13:54:02 +0400

    [Tesla.Http] Added NameValueCollection extensions.

---
+++ Tesla/Collections/NameValueCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+
+namespace Tesla.Collections
+{
+    public static class NameValueCollectionExtensions
+    {
+        public static bool HasKey(this NameValueCollection collection, string key)
+        {
+            return collection.AllKeys.Contains(key);
+        }
+
+        public static bool HasNonEmptyKey(this NameValueCollection collection, string key)
+        {
+            if (!collection.HasKeys())
+                return false;
+
+            var result = collection.Get(key);
+            return string.IsNullOrWhiteSpace(result);
+        }
+    }
+}
