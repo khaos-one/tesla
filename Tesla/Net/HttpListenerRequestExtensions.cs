3b6e7b971638c7df617dd50cd2a9b8f3eec88286
Егор Зеленский <i@kha0s.ru>
2014-06-30 13:01:37 +0400

    [Tesla.Http] Added HttpListenerRequest helpers.

---
+++ Tesla/Net/HttpListenerRequestExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesla.Net
+{
+    public static class HttpListenerRequestExtensions
+    {
+        public static bool IsGet(this HttpListenerRequest request)
+        {
+            return request.HttpMethod == "GET";
+        }
+
+        public static bool IsPost(this HttpListenerRequest request)
+        {
+            return request.HttpMethod == "POST";
+        }
+    }
+}
