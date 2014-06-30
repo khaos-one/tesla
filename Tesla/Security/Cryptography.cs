5d41647426d4adaf092a4649c91785d09bd5898e
Егор Зеленский <i@kha0s.ru>
2014-06-30 13:59:08 +0400

    [Tesla.Core] Added Cryptography feature.

---
+++ Tesla/Security/Cryptography.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesla.Security
+{
+    public static class Cryptography
+    {
+        public static byte[] ComputeSha256Hash(byte[] target, int offset = 0)
+        {
+            using (var h = SHA256.Create())
+                return h.ComputeHash(target, offset, target.Length - offset);
+        }
+    }
+}
