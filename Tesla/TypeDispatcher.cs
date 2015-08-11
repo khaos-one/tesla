ce514d40cfb55221771c9e88e18a16e869af853a
Егор Зеленский <i@khaos.su>
2015-08-11 12:32:54 +0300

    [Tesla] Add `TypeDispatcher`

---
+++ Tesla/TypeDispatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SlashCube.Server
+{
+    public class TypeDispatcher<TIn, TOut>
+        : Dictionary<Type, Func<TIn, TOut>>
+    {
+        public TOut Dispatch(object entity)
+        {
+            var type = entity.GetType();
+
+            foreach (var kv in this)
+            {
+                if (type == kv.Key)
+                {
+                    return kv.Value((TIn)entity);
+                }
+            }
+
+            return default(TOut);
+        }
+    }
+}
