b40184f9f9a824dd4e2821fcfc8d049db775f589
Егор Зеленский <i@khaos.su>
2015-09-04 16:08:57 +0300

    Add simple `Daemonizer` helper

---
+++ Tesla/Daemonizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+
+namespace Tesla
+{
+    public static class Daemonizer
+    {
+        public static void Daemonize(ThreadStart start)
+        {
+            var thread = new Thread(start);
+            thread.IsBackground = true;
+            thread.Start();
+            //Thread.CurrentThread.Abort();
+        }
+
+        public static void Daemonize(ParameterizedThreadStart start, object state)
+        {
+            var thread = new Thread(start);
+            thread.IsBackground = true;
+            thread.Start(state);
+            //Thread.CurrentThread.Abort();
+        }
+    }
+}
