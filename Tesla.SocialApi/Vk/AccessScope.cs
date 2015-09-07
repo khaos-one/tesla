899be15fd65a847af1eff489dc530cf6e5be6742
Егор Зеленский <i@khaos.su>
2015-09-07 14:54:11 +0300

    Add Tesla.SocialApi project stub

---
+++ Tesla.SocialApi/Vk/AccessScope.cs
@@ -0,0 +1,72 @@
+﻿using Tesla;
+
+namespace Tesla.SocialApi.Vk
+{
+    public enum AccessScope
+        : uint
+    {
+        None            = 0,
+
+        [EnumStringValue("notify")]
+        Notify          = 1,
+
+        [EnumStringValue("friends")]
+        Friends         = 2,
+
+        [EnumStringValue("photos")]
+        Photos          = 4,
+
+        [EnumStringValue("audio")]
+        Audio           = 8,
+
+        [EnumStringValue("video")]
+        Video           = 16,
+
+        [EnumStringValue("docs")]
+        Docs            = 131072,
+
+        [EnumStringValue("notes")]
+        Notes           = 2048,
+
+        [EnumStringValue("pages")]
+        Pages           = 128,
+
+        AddLink         = 256,
+
+        [EnumStringValue("status")]
+        Status          = 1024,
+
+        [EnumStringValue("offers")]
+        Offers          = 32,
+
+        [EnumStringValue("questions")]
+        Questions       = 64,
+
+        [EnumStringValue("wall")]
+        Wall            = 8192,
+
+        [EnumStringValue("groups")]
+        Groups          = 262144,
+
+        [EnumStringValue("messages")]
+        Messages        = 4096,
+
+        [EnumStringValue("email")]
+        Email           = 4194304,
+
+        [EnumStringValue("notifications")]
+        Notifications   = 524288,
+
+        [EnumStringValue("stats")]
+        Stats           = 1048576,
+
+        [EnumStringValue("ads")]
+        Ads             = 32768,
+
+        [EnumStringValue("offline")]
+        Offline         = 65536,
+
+        [EnumStringValue("nohttps")]
+        NoHttps
+    }
+}
---
+++ Tesla.SocialApi/Vk/VkApi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesla.SocialApi.Vk
+{
+    public sealed class VkApi
+    {
+        public const string ApiVersion = "5.34";
+
+        public ulong AppId { get; private set; }
+        public ulong UserId { get; private set; }
+        public AccessScope Scope { get; private set; }
+        public string AccessToken { get; private set; }
+
+        public VkApi(ulong appId, AccessScope scope)
+        {
+            AppId = appId;
+            Scope = scope;
+        }
+    }
+}
