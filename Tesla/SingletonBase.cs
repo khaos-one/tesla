7c7b795c29bf0c2fd5236b88c0f28ad849c5392d
Егор Зеленский <i@khaos.su>
2015-09-25 16:41:27 +0300

    Fix `SigneltonBase` to expression body

--- Tesla/SingletonBase.cs
+++ Tesla/SingletonBase.cs
@@ -3,8 +3,6 @@
         where T : class, new() {
         private static T _instance;
 
-        public static T Instance {
-            get { return _instance ?? (_instance = new T()); }
-        }
+        public static T Instance => _instance ?? (_instance = new T());
     }
 }
