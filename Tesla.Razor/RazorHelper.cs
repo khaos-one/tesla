9697416e56ea5ef8bc790288a40720df863d7d8a
Егор Зеленский <i@khaos.su>
2015-10-22 18:45:57 +0300

    Add separate Razor assembly for handling razor-related code

---
+++ Tesla.Razor/RazorHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RazorEngine;
+using RazorEngine.Templating;
+
+namespace Tesla.Razor {
+    public static class RazorHelper {
+        public static void PrecompileTemplates(string templatesDirectory, bool excludeExtensions = false) {
+            var templateFiles = Directory.GetFiles(templatesDirectory, "*.cshtml", SearchOption.AllDirectories);
+
+            foreach (var file in templateFiles) {
+                var contents = File.ReadAllText(file);
+                var templateName = file.Replace(templatesDirectory + "\\", string.Empty);
+
+                if (excludeExtensions) {
+                    templateName = templateName.Replace(".cshtml", string.Empty);
+                }
+
+                Engine.Razor.Compile(contents, templateName);
+            }
+        }
+    }
+}
