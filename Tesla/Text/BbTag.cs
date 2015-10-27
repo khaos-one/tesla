85134e8750164c903534b88c75a112d16205919c
Егор Зеленский <i@khaos.su>
2015-10-27 11:56:57 +0300

    Some changes to BbTag

--- Tesla/Text/BbTag.cs
+++ Tesla/Text/BbTag.cs
@@ -1,38 +1,64 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Tesla.Text {
     public sealed class BbTag {
-        public string OpeningPattern;
-        public string MiddlePattern;
-        public string ClosingPattern;
-        public string OpeningReplacement;
-        public string MiddleReplacement;
-        public string ClosingReplacement;
-
+        private string OverallReplacement;
+        private string OverallSearch;
         private Regex OpeningRegex;
         private Regex OverallRegex;
 
         public BbTag(string openingPattern, string middlePattern, string closingPattern, string openingReplacement, string middleReplacement, string closingReplacement) {
-            OpeningPattern = openingPattern;
-            MiddlePattern = middlePattern;
-            ClosingPattern = closingPattern;
-            OpeningReplacement = openingReplacement;
-            MiddleReplacement = middleReplacement;
-            ClosingReplacement = closingReplacement;
+            OverallReplacement = openingReplacement + middleReplacement + closingReplacement;
 
-            OpeningRegex = new Regex(OpeningPattern,
+            OpeningRegex = new Regex(openingPattern,
                 RegexOptions.IgnoreCase |
                 RegexOptions.Singleline |
                 RegexOptions.Compiled);
-            OverallRegex = new Regex(OpeningPattern + MiddlePattern + ClosingPattern, 
+            OverallRegex = new Regex(openingPattern + middlePattern + closingPattern, 
                 RegexOptions.IgnoreCase | 
                 RegexOptions.Singleline | 
                 RegexOptions.Compiled);
         }
 
+        public BbTag(string openingPattern, string closingPattern, string openingReplacement, string closingReplacement) {
+            OverallReplacement = openingReplacement + closingReplacement;
+
+            OpeningRegex = new Regex(openingPattern,
+                RegexOptions.IgnoreCase |
+                RegexOptions.Singleline |
+                RegexOptions.Compiled);
+            OverallRegex = new Regex(openingPattern + middlePattern + closingPattern,
+                RegexOptions.IgnoreCase |
+                RegexOptions.Singleline |
+                RegexOptions.Compiled);
+        }
+
+        public BbTag(string pattern, string replacement, bool regex = true) {
+            OverallReplacement = replacement;
+
+            if (regex) {
+                OverallRegex = new Regex(pattern,
+                    RegexOptions.IgnoreCase |
+                    RegexOptions.Singleline |
+                    RegexOptions.Compiled);
+            } else {
+                OverallSearch = pattern;
+            }
+        }
+
         public string Transform(string text) {
-            text = OverallRegex.Replace(text, OpeningReplacement + MiddleReplacement + ClosingReplacement);
-            text = OpeningReplacement.Replace(text, string.Empty);
+            if (OverallRegex != null) {
+                text = OverallRegex.Replace(text, OverallReplacement);
+            } else if (OverallSearch != null) {
+                text = text.Replace(OverallSearch, OverallReplacement);
+            } else {
+                throw new ArgumentException("No search pattern defined.");
+            }
+
+            if (OpeningRegex != null) {
+                text = OpeningRegex.Replace(text, string.Empty);
+            }
             // TODO: Check for unmatched opening tags and close them at the end.
 
             return text;
