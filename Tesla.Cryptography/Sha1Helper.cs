0d435ef312eea30348a58cb250fb1fb248a5ba44
Егор Зеленский <i@khaos.su>
2016-03-14 15:49:10 +0300

    Add `Sha1Helper`

---
+++ Tesla.Cryptography/Sha1Helper.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Tesla.Cryptography {
+    public static class Sha1Helper {
+        public static byte[] ComputeSha1(byte[] input) {
+            using (var sha1 = SHA1.Create()) {
+                return sha1.ComputeHash(input);
+            }
+        }
+
+        public static byte[] ComputeSha1(string input, Encoding encoding = null) {
+            if (encoding == null) {
+                encoding = Encoding.Default;
+            }
+
+            return ComputeSha1(encoding.GetBytes(input));
+        }
+    }
+}
